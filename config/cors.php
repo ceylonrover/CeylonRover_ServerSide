90d88b852a270e1339e8555f20c00ebfdcb363be
zaidATx4 <zaidnilawfer@gmail.com>
2025-09-03 20:00:02 +0530

    fix check

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://localhost:3000', 
+        env('FRONTEND_URL', 'http://localhost:3000')
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,

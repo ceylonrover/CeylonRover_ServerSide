354a9d05af3173e46c9e60ba5d47322ee8885d9f
Zaid Nilawfer <zaidnilawfer@gmail.com>
2025-09-08 23:44:32 +0530

    fix

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'https://localhost:3000', 
-        env('FRONTEND_URL', 'https://localhost:3000'),
+        env('FRONTEND_URL', 'http://localhost:3000'),
         '*' // Temporarily allow all origins for testing - remove in production
     ],
     'allowed_headers' => ['*'],

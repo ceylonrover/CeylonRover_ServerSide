e84b3e2bdb9ef56f284dae7b143e7d1023dbd606
Ahamed Naseif <ahmnaseif@gmail.com>
2025-09-08 23:22:13 +0530

    CORS

--- config/cors.php
+++ config/cors.php
@@ -6,10 +6,11 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'https://localhost:3000', 
-        env('FRONTEND_URL', 'http://localhost:3000')
+        env('FRONTEND_URL', 'https://localhost:3000'),
+        '*'  // Temporarily allow all origins for testing - remove in production
     ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => true,
 ];
